74ab6d48c78b5c684fea7312e350e3eb5b323cc0
fbazaz <fatimazahraebazaz1@gmail.com>
2023-12-18 15:41:35 +0100

    100%

---
+++ get_next_line_bonus.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbazaz <fbazaz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 11:01:58 by fbazaz            #+#    #+#             */
+/*   Updated: 2023/12/18 15:36:35 by fbazaz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_read_line(int fd, char *str)
+{
+	char	*buffer;
+	int		read_bytes;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (free(str), NULL);
+	read_bytes = 1;
+	while (read_bytes > 0)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+			return (free(buffer), free(str), NULL);
+		buffer[read_bytes] = '\0';
+		str = ft_strjoin(str, buffer);
+		if (ft_strchr(str, '\n'))
+			break ;
+	}
+	return (free(buffer), str);
+}
+
+char	*ft_fill(char *str)
+{
+	char	*p;
+	int		i;
+
+	i = 0;
+	p = NULL;
+	if (!str || str[0] == '\0')
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	p = (char *)malloc(sizeof(char) * (i + 2));
+	if (!p)
+		return (free(str), NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		p[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		p[i] = str[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+char	*ft_new_str(char *str)
+{
+	char	*p;
+	int		i;
+	int		j;
+
+	i = 0;
+	p = NULL;
+	if (!str || str[0] == '\0')
+		return (free(str), NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+		return (free(str), NULL);
+	p = (char *)malloc(sizeof(char) * (ft_strlen(str) - i + 1));
+	if (!p)
+		return (free(str), NULL);
+	i++;
+	j = 0;
+	while (str[i])
+		p[j++] = str[i++];
+	p[j] = '\0';
+	return (free(str), p);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*str[OPEN_MAX];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	str[fd] = ft_read_line(fd, str[fd]);
+	if (!str[fd])
+		return (NULL);
+	line = ft_fill(str[fd]);
+	str[fd] = ft_new_str(str[fd]);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbazaz <fbazaz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 13:33:28 by fbazaz            #+#    #+#             */
+/*   Updated: 2023/12/18 10:40:15 by fbazaz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	size_t	size;
+	int		i;
+	char	*ptr;
+
+	size = ft_strlen(str);
+	i = 0;
+	ptr = malloc((size + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	size_t	len1;
+	char	*newstring;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	len1 = ft_strlen(s1);
+	newstring = malloc((len1 + ft_strlen(s2) + 1) * sizeof(char));
+	if (!newstring)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		newstring[i] = s1[i];
+	i = -1;
+	while (s2[++i])
+	{
+		newstring[len1] = s2[i];
+		len1++;
+	}
+	newstring[len1] = '\0';
+	return (free(s1), newstring);
+}
+
+int	ft_strchr(char *s, int c)
+{
+	int		i;
+	int		len;
+	char	*str;
+
+	i = 0;
+	len = ft_strlen(s);
+	str = (char *)s;
+	while (i <= len)
+	{
+		if (str[i] == (char)c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
